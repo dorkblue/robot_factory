f684e8f1d69ac0ea18be8be5628543d7a3ef4396
Tom Lee <d.dorkblue@gmail.com>
2017-04-26 11:14:22 +0800

    modified first test

--- robot.rb
+++ robot.rb
@@ -1,3 +1,7 @@
 class Robot
-  
+  attr_accessor :name
+
+  def initialize(name = 'AB123')
+    @name = name
+  end
 end
--- spec/robot_spec.rb
+++ spec/robot_spec.rb
@@ -2,9 +2,9 @@ require_relative '../robot'
 
 describe Robot do
   describe 'Initialization' do
-    it 'should create new robot with name' do
+    it 'should create new robot name' do
       r1 = Robot.new
-      expect(r1.name.length).to exist
+      expect(r1.name).to eq('AB123')
     end
   end
 end
