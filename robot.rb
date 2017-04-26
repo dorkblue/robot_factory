aaa8adf999433a603286510e0fa93a7810d8d0bd
Lou Lee <louisa.leeyh@gmail.com>
2017-04-26 11:44:15 +0800

    add code

--- robot.rb
+++ robot.rb
@@ -1,7 +1,22 @@
 class Robot
   attr_accessor :name
-
+  $names = []
   def initialize(name = 'AB123')
-    @name = name
+    alphabets = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
+    @name = "#{alphabets[rand(0..25)]}#{alphabets[rand(0..25)]}#{rand(100..999)}"
+    if $names.include? @name
+      @name = "#{alphabets[rand(0..25)]}#{alphabets[rand(0..25)]}#{rand(100..999)}"
+    else
+      $names.push(@name)
+    end
+  end
+
+  def reset
+    @name = nil
   end
 end
+
+r1 = Robot.new
+p r1
+r2 = Robot.new
+p r2
--- spec/robot_spec.rb
+++ spec/robot_spec.rb
@@ -4,7 +4,7 @@ describe Robot do
   describe 'Initialization' do
     it 'should create new robot name' do
       r1 = Robot.new
-      expect(r1.name).to eq('AB123')
+      expect(r1.name.length).to eq(5)
     end
   end
 end
