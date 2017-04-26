9b91272bddbc94da5a9fcedb631e70ae762a974a
Tom Lee <d.dorkblue@gmail.com>
2017-04-26 11:43:25 +0800

    added test

--- spec/robot_spec.rb
+++ spec/robot_spec.rb
@@ -1,10 +1,30 @@
 require_relative '../robot'
 
 describe Robot do
+  before(:each) do
+    @r1 = Robot.new
+  end
   describe 'Initialization' do
-    it 'should create new robot name' do
-      r1 = Robot.new
-      expect(r1.name).to eq('AB123')
+    it 'should create new robot that is the instance of Robot' do
+      expect(@r1).to be_instance_of Robot
+    end
+
+    it 'should create robots with different name' do
+      @r2 = Robot.new
+      expect(@r1.name).not_to eq(@r2.name)
+    end
+  end
+
+  describe 'Reset' do
+    it 'should create new name for robot' do
+      @r1.reset
+      expect(@r1.key?(:name)).to eq true
+    end
+
+    it 'should create new name that is different from previous name' do
+      old_name = @r1.name
+      @r1.reset
+      expect(@r1.name).not_to eq(old_name)
     end
   end
 end
